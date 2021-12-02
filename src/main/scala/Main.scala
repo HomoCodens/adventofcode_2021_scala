ca8279a05ba77fca0d24b8ae305f9941eca4b5bd
thoenis <sthoeni@kof.ethz.ch>
2021-12-02 08:35:05 +0100

    solve day 2
    up next: bring some structure to this thing

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -13,4 +13,48 @@ object Main extends App {
   def doit(x : Array[Int]) : Int = {
     x.sliding(2).map(a => a(0) < a(1)).count(a => a)
   }
+
+  def parseInstruction(x : String) : Tuple2[String, Int] = {
+    val parts = x.split(" ")
+    (parts(0), parts(1).toInt)
+  }
+
+  val instructions = scala.io.Source.fromFile("inputs/day2/input.txt").getLines.toArray.map(parseInstruction)
+  var h = 0
+  var d = 0
+  for(x <- instructions) {
+    if(x._1 == "forward") {
+      println("going forward")
+      h += x._2
+    } else if(x._1 == "down") {
+      println("going down")
+      d += x._2
+    } else {
+      println("going up")
+      d -= x._2
+    }
+  }
+  println(h)
+  println(d)
+  println(h*d)
+
+  h = 0
+  d = 0
+  var a = 0
+  for(x <- instructions) {
+    if(x._1 == "forward") {
+      println("going forward")
+      h += x._2
+      d += a * x._2
+    } else if(x._1 == "down") {
+      println("going down")
+      a += x._2
+    } else {
+      println("going up")
+      a -= x._2
+    }
+  }
+  println(h)
+  println(d)
+  println(h * d)
 }
