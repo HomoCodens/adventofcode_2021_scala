5c8228596523f44b937489a097aef630760f3acf
thoenis <sthoeni@kof.ethz.ch>
2021-12-03 19:14:38 +0100

    port day 3

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 import aocutil.InputReader
 import solvers.Day1Solver
 import solvers.Day2Solver
+import solvers.Day3Solver
 object Main extends App {
 
   val d1 = new Day1Solver("./inputs", false)
@@ -9,69 +10,6 @@ object Main extends App {
   val d2 = new Day2Solver("./inputs", false)
   d2.run()
 
-  /*val day3Reader = new InputReader[List[Int]]("./inputs", 3)
-
-  def parse(x: String) : List[Int] = {
-    x.split("").toList.map(_.toInt)
-  }
-
-  val diagnostics = day3Reader.readParsed(parse)
-  for(x <- diagnostics) {
-    println(x)
-  }
-
-  val threshold = diagnostics.length / 2
-  val counts = diagnostics.reduce((a, b) => a.zip(b).map(x => x._1 + x._2))
-  println(counts)
-  val gamma = Integer.parseInt(counts.map(x => if (x > threshold) 1 else 0).mkString, 2)
-  println(gamma)
-  val epsilon = Integer.parseInt(counts.map(x => if (x < threshold) 1 else 0).mkString, 2)
-  println(epsilon)
-  println(epsilon * gamma)
-
-  def lifeSupportFilter(inputs: List[List[Int]], digit: Int, ogr: Boolean) = {
-    val threshold: Double = inputs.length / 2.0
-    val counts = inputs.reduce((a, b) => a.zip(b).map(x => x._1 + x._2))
-    println(counts)
-    println(threshold)
-    if(ogr) {
-      if(counts(digit) >= threshold) {
-        inputs.filter(x => x(digit) == 1)
-      } else {
-        inputs.filter(x => x(digit) == 0)
-      }
-    } else {
-      if(counts(digit) < threshold) {
-        inputs.filter(x => x(digit) == 1)
-      } else {
-        inputs.filter(x => x(digit) == 0)
-      }
-    }
-  }
-
-  var bla = diagnostics
-  var di = 0
-  while(bla.length > 1) {
-    bla = lifeSupportFilter(bla, di, true)
-    di += 1
-    println(bla)
-    println("======================")
-  }
-  println(bla)
-
-  var blu = diagnostics
-  di = 0
-  while(blu.length > 1) {
-    blu = lifeSupportFilter(blu, di, false)
-    di += 1
-    println(blu)
-    println("==========================")
-  }
-  println(blu)
-
-  val ogr = Integer.parseInt(bla(0).mkString, 2)
-  val co2 = Integer.parseInt(blu(0).mkString, 2)
-  println(ogr)
-  println(co2)
-  println(ogr * co2)*/
+  val d3 = new Day3Solver("./inputs", false)
+  d3.run()
 }
---
+++ src/main/scala/solvers/Day3Solver.scala
@@ -0,0 +1,65 @@
+package solvers
+import aocutil.InputReader
+
+class Day3Solver(inputRoot: String,
+                 verbose: Boolean = false,
+                 test: Boolean = false,
+                 testCase: Int = 1) extends Solver(inputRoot, verbose) {
+    val day = 3
+    type Diagnostic = List[Int]
+    val diagnostics = {        
+        val reader = new InputReader[Diagnostic](inputRoot, day)
+        reader.readParsed(_.split("").toList.map(_.toInt), test, testCase)
+    }
+
+    def getBitCounts(diagnostics: List[Diagnostic]) : List[Int] = {
+        diagnostics.reduce((a, b) => a.zip(b).map({ case (acc, x) => acc + x}))
+    }
+
+    def bitsToInt(bits: List[Boolean]) : Int = {
+        Integer.parseInt(bits.map(if (_) 1 else 0).mkString, 2)
+    }
+
+    def filterLifeSupport(diagnostics: List[Diagnostic], digit: Int, ogr: Boolean) : List[Diagnostic] = {
+        val threshold = diagnostics.length / 2.0f
+        val bitCount = getBitCounts(diagnostics)(digit)
+        val keep = if(ogr) {
+            // Woulda worked more elegantly with booleans
+            if(bitCount >= threshold) 1 else 0
+        } else {
+            if(bitCount< threshold) 1 else 0
+        }
+        diagnostics.filter(_(digit) == keep)
+    }
+
+    // Typoed o twice, kept it for the german lulz
+    def runFolter(diagnostics: List[Diagnostic], ogr: Boolean) : Int = {
+        var diag = diagnostics
+        var digit = 0
+        while(diag.length > 1 && digit < diag(0).length) {
+            diag = filterLifeSupport(diag, digit, ogr)
+            digit += 1
+        }
+        if(diag.length > 1) {
+            throw new RuntimeException("Summat went wrong")
+        }
+        bitsToInt(diag(0).map(_ == 1))
+    }
+    override def part1() : String = {
+        val threshold: Float = diagnostics.length / 2.0f
+        val counts = getBitCounts(diagnostics)
+        val gamma = bitsToInt(counts.map(_ >= threshold))
+        val epsilon = bitsToInt(counts.map(_ < threshold))
+        p(s"Gamma is $gamma")
+        p(s"Epsilon is $epsilon")
+        (gamma * epsilon).toString
+    }
+
+    override def part2() : String = {
+        val ogr = runFolter(diagnostics, true)
+        val co2 = runFolter(diagnostics, false)
+        p(s"Oxygen generator rating: $ogr")
+        p(s"CO2 scrubber rating: $co2")
+        (ogr * co2).toString
+    }
+}
