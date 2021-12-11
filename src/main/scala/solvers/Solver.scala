7f707cc0943b16b0819b89314cb5c74aebfc07f0
thoenis <sthoeni@kof.ethz.ch>
2021-12-11 13:00:35 +0100

    add timing (and regretting it)

--- src/main/scala/solvers/Solver.scala
+++ src/main/scala/solvers/Solver.scala
@@ -13,10 +13,16 @@ abstract class Solver(inputRoot: String, verbose: Boolean = false, test: Boolean
         println("==========================")
         println(s"Day $day")
         println("==========================")
+        val p1t0 = System.nanoTime()
         val p1 = part1()
+        val p1t = System.nanoTime() - p1t0
+        val p2t0 = System.nanoTime()
         val p2 = part2()
+        val p2t = System.nanoTime() - p2t0
         println(s"Part 1: $p1")
+        println(s"Part 1 ran in ${p1t.toFloat/1000000}ms")
         println(s"Part 2: $p2")
+        println(s"Part 2 ran in ${p2t.toFloat/1000000}ms")
         println()
         println()
     }
