149844c7fd4195e63e908df0cba1796303f620c1
thoenis <sthoeni@kof.ethz.ch>
2021-12-20 21:43:21 +0100

    go over day 3

--- src/main/scala/solvers/Day3Solver.scala
+++ src/main/scala/solvers/Day3Solver.scala
@@ -10,7 +10,7 @@ class Day3Solver(inputRoot: String,
     type Diagnostic = List[Int]
     val diagnostics = {        
         val reader = new InputReader[Diagnostic](inputRoot, day)
-        reader.readParsedByLine(_.split("").toList.map(_.toInt), test, testCase)
+        reader.readParsedByLine(_.split("").map(_.toInt).toList, test, testCase)
     }
 
     def getBitCounts(diagnostics: List[Diagnostic]) : List[Int] = {
@@ -28,23 +28,22 @@ class Day3Solver(inputRoot: String,
             // Woulda worked more elegantly with booleans
             if(bitCount >= threshold) 1 else 0
         } else {
-            if(bitCount< threshold) 1 else 0
+            if(bitCount < threshold) 1 else 0
         }
         diagnostics.filter(_(digit) == keep)
     }
 
     // Typoed o twice, kept it for the german lulz
-    def runFolter(diagnostics: List[Diagnostic], ogr: Boolean) : Int = {
-        var diag = diagnostics
-        var digit = 0
-        while(diag.length > 1 && digit < diag(0).length) {
-            diag = filterLifeSupport(diag, digit, ogr)
-            digit += 1
-        }
-        if(diag.length > 1) {
+    def runFolter(diagnostics: List[Diagnostic], ogr: Boolean, digit: Int = 0) : Int = {
+        if(digit > diagnostics(0).length) {
             throw new RuntimeException("Summat went wrong")
         }
-        bitsToInt(diag(0).map(_ == 1))
+
+        if(diagnostics.length == 1) {
+            bitsToInt(diagnostics(0).map(_ == 1))
+        } else {
+            runFolter(filterLifeSupport(diagnostics, digit, ogr), ogr, digit + 1)
+        }
     }
     override def part1() : String = {
         val threshold: Float = diagnostics.length / 2.0f
@@ -53,7 +52,7 @@ class Day3Solver(inputRoot: String,
         val epsilon = bitsToInt(counts.map(_ < threshold))
         p(s"Gamma is $gamma")
         p(s"Epsilon is $epsilon")
-        (gamma * epsilon).toString
+        s"Power consumption is ${gamma*epsilon}"
     }
 
     override def part2() : String = {
@@ -61,6 +60,6 @@ class Day3Solver(inputRoot: String,
         val co2 = runFolter(diagnostics, false)
         p(s"Oxygen generator rating: $ogr")
         p(s"CO2 scrubber rating: $co2")
-        (ogr * co2).toString
+        s"Life support rating is ${ogr*co2}"
     }
 }
