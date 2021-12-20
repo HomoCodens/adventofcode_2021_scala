885fbee5a9eb69533d626ddaf5b1ef545ff60b4f
thoenis <sthoeni@kof.ethz.ch>
2021-12-20 21:28:26 +0100

    go over day 2

--- src/main/scala/solvers/Day2Solver.scala
+++ src/main/scala/solvers/Day2Solver.scala
@@ -12,27 +12,24 @@ class Day2Solver(inputRoot: String,
     val instructions = {
         val day2Reader = new InputReader[Instruction](inputRoot, day)
 
-        def parseInstruction(x : String) : Instruction = {
+        def parseInstruction(x: String) : Instruction = {
             x.split(" ") match {
                 case Array(dir, mag) => (dir.substring(0, 1), mag.toInt)
             }
         }
         
-        day2Reader.readParsedByLine(parseInstruction)
+        day2Reader.readParsedByLine(parseInstruction, test, testCase)
     }
     
     override def part1() : String = {
-        val (h, d) = instructions.foldLeft((0, 0))((acc, instr) => {
-            // Shame I haven't found a way to destruct those both at the same time
-            val (i, m) = instr
-            val (h, d) = acc
-            i match {
-                case "f" => (h + m, d)
-                case "d" => (h, d + m)
-                case "u" => (h, d - m)
-            }
+        val (h, d) = instructions.foldLeft((0, 0))({
+            case ((h, d), ("f", m)) => (h + m, d)
+            case ((h, d), ("d", m)) => (h, d + m)
+            case ((h, d), ("u", m)) => (h, d - m)
         })
-        (h*d).toString
+        p(s"Horizontal position: $h")
+        p(s"Depth: $d")
+        s"Diving right angle like: ${h*d}"
     }
 
 
@@ -46,6 +43,8 @@ class Day2Solver(inputRoot: String,
                 case "u" => (h, d, a - m)
             }
         })
-        (h*d).toString
+        p(s"Horizontal position: $h")
+        p(s"Depth: $d")
+        s"Diving with aim: ${h*d}"
     }
 }
